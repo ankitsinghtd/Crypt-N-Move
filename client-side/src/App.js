b39ef3de22db36bdd3c1aa6db8c673d607cb27aa
anushkabishnoi <anushkabishnoi9@gmail.com>
2024-04-01 12:37:33 +0530

    added about page/path

--- client-side/src/App.js
+++ client-side/src/App.js
@@ -1,17 +1,21 @@
 import Home from "./pages/Home";
 import Send from "./pages/Send";
 import Receive from "./pages/Receive";
+import About from "./pages/About";
 
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div className="App">
+      <Navbar/>
       <Router>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/send" element={<Send />} />
           <Route path="/receive" element={<Receive />} />
+          <Route path="/about" element={<About />} />
         </Routes>
       </Router>
     </div>
