2e497153f0de0118bf439374f3e221592635cd0c
anushkabishnoi <anushkabishnoi9@gmail.com>
2024-04-01 14:32:05 +0530

    navbar changes

--- client-side/src/components/Navbar.jsx
+++ client-side/src/components/Navbar.jsx
@@ -1,66 +1,23 @@
-import React, { useState } from "react";
-import './navbar.css'
+import "./navbar.css";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
-  const [isMenuOpen, setIsMenuOpen] = useState(false);
-
-  const toggleMenu = () => {
-    setIsMenuOpen(!isMenuOpen);
-  };
-
   return (
     <>
       <nav id="desktop-nav">
-        <div className="logo">Cyrpt-n-Move</div>
+        <div className="logo">
+          {/* <Link to="/">
+            <span>Cyrpt-n-Move</span>
+          </Link> */}
+        </div>
         <div>
           <ul className="nav-links">
             <li>
-              <a href="/">Home</a>
-            </li>
-            <li>
-              <a href="#">Tech</a>
-            </li>
-            <li>
-              <a href="#">About</a>
-            </li>
-            <li>
-              <a href="#">Contact</a>
+              {/* <Link to="/about">
+                <span>About</span>
+              </Link> */}
             </li>
           </ul>
-        </div>
-      </nav>
-      <nav id="hamburger-nav">
-        <div className="logo">VoteChain</div>
-        <div className="hamburger-menu">
-          <div className="hamburger-icon" onClick={toggleMenu}>
-            <span></span>
-            <span></span>
-            <span></span>
-          </div>
-          <div className={`menu-links ${isMenuOpen ? "open" : ""}`}>
-            <ul>
-              <li>
-                <a href="#" onClick={toggleMenu}>
-                  Home
-                </a>
-              </li>
-              <li>
-                <a href="/src/pages/polls.html" onClick={toggleMenu}>
-                  Polls
-                </a>
-              </li>
-              <li>
-                <a href="#" onClick={toggleMenu}>
-                  About
-                </a>
-              </li>
-              <li>
-                <a href="#" onClick={toggleMenu}>
-                  Contact
-                </a>
-              </li>
-            </ul>
-          </div>
         </div>
       </nav>
     </>
--- client-side/src/pages/Home.jsx
+++ client-side/src/pages/Home.jsx
@@ -1,48 +1,37 @@
 // import { useState } from "react";
 import "./home.css";
 import "../components/Navbar";
-import Navbar from "../components/Navbar";
-import { useState } from "react";
 import { Link } from "react-router-dom";
 
 function Home() {
-
-
-  // const [isDarkMode, setIsDarkMode] = useState(false);
-  // const toggleDarkMode = () => {
-  //   setIsDarkMode(!isDarkMode);
-  // };
-
   return (
-    // <div className={isDarkMode ? "dark-mode" : ""}>
-    <div>
-      <Navbar />
-      <main className="main-content">
-        <h1>Securing Transfers, One P2P Encryption at a Time.</h1>
-        <h2>Quick . Safe . Secure</h2>
+    <>
+      <div className="home-container">
+        <main className="main-content">
+          <h1>Securing Transfers, One P2P Encryption at a Time.</h1>
+          <h2>Quick . Safe . Secure</h2>
 
-        <div className="usp-heading">
-          <h1>Crypt-n-Move: </h1>
-          <h1>Redefining File Transfers.</h1>
+          <div className="usp-heading">
+            <h1>Crypt-n-Move: </h1>
+            <h1>Redefining File Transfers.</h1>
 
-          <div className="home-buttons">
-            <Link to="/send">
-              <button className="make-a-vote">
-                <span>Send</span>
-              </button>
-            </Link>
-            {/* enter the link to send  page*/}
-
-            <Link to="/receive">
-
-              <button className="create-a-poll">
-                <span>Receive</span>
-              </button>
-            </Link>
+            <div className="home-buttons">
+              <Link to="/send">
+                <button className="make-a-vote">
+                  <span>Send</span>
+                </button>
+              </Link>
+              {/* enter the link to send  page*/}
+              <Link to="/receive">
+                <button className="create-a-poll">
+                  <span>Receive</span>
+                </button>
+              </Link>
+            </div>
           </div>
-        </div>
-      </main>
-    </div>
+        </main>
+      </div>
+    </>
   );
 }
 
